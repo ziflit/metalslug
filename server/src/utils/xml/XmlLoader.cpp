ffa8c9ec7c4abd30f0933fcf8a73ba9297d1c7cb
Santi <santimontiel@yahoo.com>
2016-10-06 16:18:21 -0300

    Agrego parser del XML

--- server/src/utils/xml/XmlLoader.cpp
+++ server/src/utils/xml/XmlLoader.cpp
@@ -1,43 +1,57 @@
-/*
- * XmlLoader.cpp
- *
- *  Created on: 02/10/2016
- *      Author: fpirra
- */
-
 #include "XmlLoader.h"
 
 using namespace std;
 using namespace rapidxml;
 
 XmlLoader::XmlLoader() {
-	// TODO Auto-generated constructor stub
+
 
 }
 
 XmlLoader::~XmlLoader() {
-	// TODO Auto-generated destructor stub
+
 }
 
-//TODO: Hacer que lea el XML posta -------
 void XmlLoader::loadXML(){
+
 	xml_document<> doc;
-	file<> xmlFile("../../config.xml"); //open file
+	file<> xmlFile("../../../config.xml"); //open file
 	doc.parse<0>(xmlFile.data()); //parse the contents of file
 
-	xml_node<>* root = doc.first_node("ventana");
-	for(xml_node<>* cd = root->first_node("CD"); cd ; cd=cd->next_sibling()){
+	xml_node<>* ventana = doc.first_node("ventana");
+	xml_node<>* ancho = ventana->first_node("ancho");
+	xml_node<>* alto = ventana->first_node("alto");
+	cout << ancho->value() << endl;
+	cout << alto->value() << endl;
 
-		xml_node<>* title = cd->first_node("TITULO");
-		xml_node<>* artist = cd->first_node("ARTISTA");
-		xml_node<>* origen = cd->first_node("ORIGEN");
-		xml_node<>* precio = cd->first_node("PRECIO");
-		xml_node<>* ano = cd->first_node("ANO");
-		cout << title->value() << "\n";
-		cout << artist->value() << "\n"; //print node1 value
-		cout << origen->value() << "\n";
-		cout << precio->value() << "\n";
-		cout << ano->value() << "\n";
+	xml_node<>* sprites = doc.first_node("sprites");
 
+	for(xml_node<>* sprite = sprites->first_node("sprite"); sprite ; sprite = sprite->next_sibling()){
+
+		xml_node<>* id = sprite->first_node("id");
+		xml_node<>* path = sprite->first_node("path");
+		xml_node<>* ancho = sprite->first_node("ancho");
+		xml_node<>* alto = sprite->first_node("alto");
+		xml_node<>* zindex = sprite->first_node("alto");
+
+		cout << id->value() << endl;
+		cout << path->value() << endl;
+		cout << ancho->value() << endl;
+		cout << alto->value() << endl;
+		cout << zindex->value() << endl;
 	}
+	xml_node<>* capas = doc.first_node("capas");
+
+	for(xml_node<>* capa = capas->first_node("capa"); capa ; capa = capa->next_sibling()){
+
+		xml_node<>* imagen_fondo = capa->first_node("imagen_fondo");
+		xml_node<>* ancho = capa->first_node("ancho");
+		xml_node<>* zindex = capa->first_node("zindex");
+
+		cout << imagen_fondo->value() << endl;
+		cout << ancho->value() << endl;
+		cout << zindex->value() << endl;
+	}
+
 }
+
