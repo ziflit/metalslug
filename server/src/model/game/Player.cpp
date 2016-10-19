23e8173371b89f6499c20d2cdb773307cc6e9c47
mfprado <pradomflorencia@gmail.com>
2016-10-19 02:44:22 -0300

    si esta agachado no salta

--- server/src/model/game/Player.cpp
+++ server/src/model/game/Player.cpp
@@ -116,49 +116,51 @@ void Player::handleRealeasedKey(EventCode nuevoEvento) {
 void Player::handlePressedKey(EventCode nuevoEvento){
     switch (nuevoEvento){
         case EventCode::SDL_KEY_A_PRESSED:
-            direccionY = 1;
+            if (!((postura == AGACHADO_MIRANDO_DERECHA_QUIETO) or (postura == AGACHADO_AVANZANDO_DERECHA) or (postura == AGACHADO_MIRANDO_IZQUIERDA_QUIETO) or (postura == AGACHADO_AVANZANDO_IZQUIERDA) )){
+                direccionY = 1;
+            }
             break;
 
         case EventCode::SDL_KEYUP_PRESSED:
-            if(direccionX == 1){postura = Postura::MIRANDO_ARRIBA_CAMINANDO_DERECHA;}
-            else if(direccionX == -1){postura = Postura::MIRANDO_ARRIBA_CAMINANDO_IZQUIERDA;}
-            else if(postura == Postura::MIRANDO_IZQUIERDA_QUIETO and direccionX == 0){postura = Postura::MIRANDO_ARRIBA_IZQUIERDA_QUIETO;}
-            else {postura = Postura::MIRANDO_ARRIBA_DERECHA_QUIETO;}
+            if(direccionX == 1){postura =  MIRANDO_ARRIBA_CAMINANDO_DERECHA;}
+            else if(direccionX == -1){postura =  MIRANDO_ARRIBA_CAMINANDO_IZQUIERDA;}
+            else if(postura ==  MIRANDO_IZQUIERDA_QUIETO and direccionX == 0){postura =  MIRANDO_ARRIBA_IZQUIERDA_QUIETO;}
+            else {postura =  MIRANDO_ARRIBA_DERECHA_QUIETO;}
             break;
 
         case EventCode::SDL_KEYDOWN_PRESSED:
-            if(direccionX == 1){postura = Postura::AGACHADO_AVANZANDO_DERECHA;}
-            else if(direccionX == -1){postura = Postura::AGACHADO_AVANZANDO_IZQUIERDA;}
-            else if(postura == Postura::MIRANDO_IZQUIERDA_QUIETO and direccionX == 0){postura = Postura::AGACHADO_MIRANDO_IZQUIERDA_QUIETO;}
-            else {postura = Postura::AGACHADO_MIRANDO_DERECHA_QUIETO;}
+            if(direccionX == 1){postura =  AGACHADO_AVANZANDO_DERECHA;}
+            else if(direccionX == -1){postura =  AGACHADO_AVANZANDO_IZQUIERDA;}
+            else if(postura ==  MIRANDO_IZQUIERDA_QUIETO and direccionX == 0){postura =  AGACHADO_MIRANDO_IZQUIERDA_QUIETO;}
+            else {postura =  AGACHADO_MIRANDO_DERECHA_QUIETO;}
             break;
 
         case EventCode::SDL_KEYLEFT_PRESSED:
             if(direccionX == 0){
-                if((postura == Postura::MIRANDO_ARRIBA_IZQUIERDA_QUIETO) or (postura == Postura::MIRANDO_ARRIBA_DERECHA_QUIETO)){postura = Postura::MIRANDO_ARRIBA_CAMINANDO_IZQUIERDA;}
-                else if((postura == Postura::AGACHADO_MIRANDO_IZQUIERDA_QUIETO) or (postura == Postura::AGACHADO_MIRANDO_DERECHA_QUIETO)){postura = Postura::AGACHADO_AVANZANDO_IZQUIERDA;}
-                else if((postura == Postura::MIRANDO_IZQUIERDA_QUIETO) or (postura == Postura::MIRANDO_DERECHA_QUIETO)){postura = Postura::CAMINANDO_IZQUIERDA; }
-                else{postura = Postura::CAMINANDO_IZQUIERDA;}
+                if((postura ==  MIRANDO_ARRIBA_IZQUIERDA_QUIETO) or (postura ==  MIRANDO_ARRIBA_DERECHA_QUIETO)){postura =  MIRANDO_ARRIBA_CAMINANDO_IZQUIERDA;}
+                else if((postura ==  AGACHADO_MIRANDO_IZQUIERDA_QUIETO) or (postura ==  AGACHADO_MIRANDO_DERECHA_QUIETO)){postura =  AGACHADO_AVANZANDO_IZQUIERDA;}
+                else if((postura ==  MIRANDO_IZQUIERDA_QUIETO) or (postura ==  MIRANDO_DERECHA_QUIETO)){postura =  CAMINANDO_IZQUIERDA; }
+                else{postura =  CAMINANDO_IZQUIERDA;}
                 direccionX = -1;
             }
             /* Si está caminando hacia la derecha y presiona LEFT entonces queda quieto */
             else if(direccionX == 1){
                 direccionX = 0;
-                postura = Postura::MIRANDO_DERECHA_QUIETO;}
+                postura =  MIRANDO_DERECHA_QUIETO;}
             break;
 
         case EventCode::SDL_KEYRIGHT_PRESSED:
             if(direccionX == 0){
-                if((postura == Postura::MIRANDO_ARRIBA_IZQUIERDA_QUIETO) or (postura == Postura::MIRANDO_ARRIBA_DERECHA_QUIETO)){postura = Postura::MIRANDO_ARRIBA_CAMINANDO_DERECHA;}
-                else if((postura == Postura::AGACHADO_MIRANDO_IZQUIERDA_QUIETO) or (postura == Postura::AGACHADO_MIRANDO_DERECHA_QUIETO)){postura = Postura::AGACHADO_AVANZANDO_DERECHA;}
-                else if((postura == Postura::MIRANDO_IZQUIERDA_QUIETO) or (postura == Postura::MIRANDO_DERECHA_QUIETO)){postura = Postura::CAMINANDO_DERECHA; }
-                else{postura = Postura::CAMINANDO_DERECHA;}
+                if((postura ==  MIRANDO_ARRIBA_IZQUIERDA_QUIETO) or (postura ==  MIRANDO_ARRIBA_DERECHA_QUIETO)){postura =  MIRANDO_ARRIBA_CAMINANDO_DERECHA;}
+                else if((postura ==  AGACHADO_MIRANDO_IZQUIERDA_QUIETO) or (postura ==  AGACHADO_MIRANDO_DERECHA_QUIETO)){postura =  AGACHADO_AVANZANDO_DERECHA;}
+                else if((postura ==  MIRANDO_IZQUIERDA_QUIETO) or (postura ==  MIRANDO_DERECHA_QUIETO)){postura =  CAMINANDO_DERECHA; }
+                else{postura =  CAMINANDO_DERECHA;}
                 direccionX = 1;
             }
             /* Si está caminando hacia la izquierda y presiona RIGHT entonces queda quieto */
             else if(direccionX == -1){
                 direccionX = 0;
-                postura = Postura::MIRANDO_IZQUIERDA_QUIETO;}
+                postura =  MIRANDO_IZQUIERDA_QUIETO;}
             break;
 
         default:
@@ -169,7 +171,7 @@ void Player::handlePressedKey(EventCode nuevoEvento){
 
 void Player::updateState(EventCode nuevoEvento){
     if (nuevoEvento == EventCode::CLIENT_DISCONNECT){
-        this->postura = Postura::DESCONECTADO;
+        this->postura =  DESCONECTADO;
     }
     else if (isKeyPressed(nuevoEvento)) {
         handlePressedKey(nuevoEvento);
@@ -225,7 +227,6 @@ struct event Player::getNewState(){
 	eventExt.x = x;  //Actualizo la posicion del player
 	eventExt.y = y;
     eventExt.postura = this->postura;
-
 
     estado.completion = EventCompletion::PARTIAL_MSG;
 	estado.data = eventExt;
