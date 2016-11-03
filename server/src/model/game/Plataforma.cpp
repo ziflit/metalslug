84c17adc63c1f4f14884298bb68fd183813dc7b1
Francisco Pirra <fpirra@fi.uba.ar>
2016-11-03 16:30:32 -0300

    Agrego clases colisionador y plataforma

---
+++ server/src/model/game/Plataforma.cpp
@@ -0,0 +1,18 @@
+/*
+ * Plataforma.cpp
+ *
+ *  Created on: Nov 3, 2016
+ *      Author: fpirra
+ */
+
+#include "Plataforma.h"
+
+Plataforma::Plataforma() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Plataforma::~Plataforma() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ server/src/model/game/Plataforma.h
@@ -0,0 +1,18 @@
+/*
+ * Plataforma.h
+ *
+ *  Created on: Nov 3, 2016
+ *      Author: fpirra
+ */
+
+#ifndef SRC_MODEL_GAME_PLATAFORMA_H_
+#define SRC_MODEL_GAME_PLATAFORMA_H_
+
+class Plataforma : public GameObject {
+
+public:
+	Plataforma();
+	virtual ~Plataforma();
+};
+
+#endif /* SRC_MODEL_GAME_PLATAFORMA_H_ */
