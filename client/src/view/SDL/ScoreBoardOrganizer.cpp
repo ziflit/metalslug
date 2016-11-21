9d793549885018d0774fc993f6b4c6fd84e153f3
mfprado <pradomflorencia@gmail.com>
2016-11-21 19:21:50 -0300

    No estaba actualizando los puntos cuando terminaba nivel 2 o 3 en la scoreBoard

--- client/src/view/SDL/ScoreBoardOrganizer.cpp
+++ client/src/view/SDL/ScoreBoardOrganizer.cpp
@@ -21,11 +21,18 @@ void ScoreBoardOrganizer::realize() {
 
 void ScoreBoardOrganizer::setData(vector<PlayerSprite *> playersSprites) {
     this->renderize = true;
+    if(usernameTexts.size() > 0){
+        for (int i = 0 ; i < usernameTexts.size() ; i++) {
+            usernameTexts[i]->changeText(playersSprites[i]->getUsername());
+            puntajeTexts[i]->changeText(playersSprites[i]->getPuntaje());
+        }
+        return;
+    }
     for (int i = 0 ; i < scoreBoardSprites.size() ; i++ ) {
         PlayerSprite* player = playersSprites[i];
 
-        usernameTexts.push_back(new TextBox(player->getUsername(), player->getRenderer(), {0,0,255,0},25));
-        puntajeTexts.push_back(new TextBox(player->getPuntaje(),player->getRenderer(),{0,0,255,0},25));
+        usernameTexts.push_back(new TextBox(player->getUsername(), player->getRenderer(), {0,0,255,0},28));
+        puntajeTexts.push_back(new TextBox(player->getPuntaje(),player->getRenderer(),{0,0,255,0},28));
     }
     this->setTextPositions();
 }
