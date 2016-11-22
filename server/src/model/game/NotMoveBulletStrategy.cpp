5da625297992a0b41843ec098c83bc99af8bb78f
Francisco Pirra <fpirra@fi.uba.ar>
2016-11-22 20:11:39 -0300

    ShotGun funcionando bien
    Desaparecen las balas, habria que poner un sprite con mas pinta de SHOTGUN jajaja

--- server/src/model/game/NotMoveBulletStrategy.cpp
+++ server/src/model/game/NotMoveBulletStrategy.cpp
@@ -2,5 +2,15 @@
 #include "NotMoveBulletStrategy.h"
 
 void NotMoveBulletStrategy::avanzar(vector<GameObject *> collisionables, GameObject *bullet) {
-    makeCollision(collisionables, bullet);
+    Bullet *bulletObject = (Bullet *) bullet;
+    if (bulletObject->getCantPasos() < 3){
+        float x = bullet->getX() + bullet->getDireccionX() * bullet->getSpeed();
+        float y = bullet->getY() - bullet->getDireccionY() * bullet->getSpeed();
+        bullet->setX(x);
+        bullet->setY(y);
+        makeCollision(collisionables, bullet);
+        bulletObject->updateCantPasos(1);
+    } else {
+        bullet->setEntity(DEAD);
+    }
 }
