361b0fbfcfc7a23a6d8e893058b6bdb1756a9bd2
mfprado <pradomflorencia@gmail.com>
2016-11-23 19:06:51 -0300

    Saco couts

--- client/src/view/SDL/spriteClasses/Sprite.cpp
+++ client/src/view/SDL/spriteClasses/Sprite.cpp
@@ -11,14 +11,14 @@ SDL_Texture* Sprite::loadTexture(string imageTexturePath){
     SDL_Texture* backgroundTexture = NULL;
     SDL_Surface* loadingSurface = IMG_Load(imageTexturePath.c_str());
     if(loadingSurface == NULL){
-        cout<<"Error: can't load image which path is: "<<imageTexturePath.c_str()<<endl;
+//        cout<<"Error: can't load image which path is: "<<imageTexturePath.c_str()<<endl;
         loadingSurface = IMG_Load("sprites/defaultImage.png");
     }
 
 
     backgroundTexture = SDL_CreateTextureFromSurface(renderer, loadingSurface);
 
-    if(backgroundTexture == NULL) cout<<"Error creating background layer: "<<SDL_GetError()<<endl;
+//    if(backgroundTexture == NULL) cout<<"Error creating background layer: "<<SDL_GetError()<<endl;
 
     SDL_FreeSurface(loadingSurface);    //get rid of old loaded surface
     return backgroundTexture;
